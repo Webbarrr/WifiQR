a80b0859b73a1f676e0e0d3e565595b051175f10
james <jameswebber90@gmail.com>
2025-09-06 18:47:15 +0100

    Add project files.

---
+++ WifiQR-CLI/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WifiQR_CLI;
+
+public class AppSettings
+{
+    public string SSID { get; set; }
+    public string Password { get; set; }
+    public string Auth { get; set; }
+    public bool Hidden { get; set; }
+}
---
+++ WifiQR-CLI/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using QRCoder;
+
+namespace WifiQR_CLI;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+
+            var settings = config.GetSection(nameof(AppSettings)).Get<AppSettings>();
+            if (settings is null)
+            {
+                throw new Exception("No appsettings found.");
+            }
+
+            string wifiPayload = $"WIFI:T:{settings.Auth};S:{settings.SSID};P:{settings.Password};H:{(settings.Hidden ? "true" : "")};;";
+
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrCodeData = qrGenerator.CreateQrCode(wifiPayload, QRCodeGenerator.ECCLevel.Q);
+
+            var qrCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeBytes = qrCode.GetGraphic(20);
+
+            var outputName = "wifiqr.png";
+            File.WriteAllBytes(outputName, qrCodeBytes);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"QR code saved to {outputName}");
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ResetColor();
+        }
+    }
+}
